f7c7f8d78d5e0e4aa6a68a0622e4f5e22ae28e01
mithulcb <78469903+mithulcb@users.noreply.github.com>
2022-06-07 13:52:49 +0530

    Add files via upload

---
+++ chat_bot.py
@@ -0,0 +1,68 @@
+from nltk.chat.util import Chat, reflections
+pairs = [
+    [
+        r"(.*)my name is (.*)",
+        ["Hello %2, How are you today ?",]
+    ],
+    [
+        r"(.*)help(.*) ",
+        ["I can help you ",]
+    ],
+     [
+        r"(.*) your name ?",
+        ["You can just call me robot and I'm a chatbot .",]
+    ],
+    [
+        r"how are you (.*) ?",
+        ["I'm doing very well", "i am great !"]
+    ],
+    [
+        r"sorry (.*)",
+        ["Its alright","Its OK, never mind that",]
+    ],
+    [
+        r"i'm (.*) (good|well|okay|ok)",
+        ["Nice to hear that","Alright, great !",]
+    ],
+    [
+        r"(hi|hey|hello|hola|holla)(.*)",
+        ["Hello", "Hey there",]
+    ],
+    [
+        r"what (.*) want ?",
+        ["Make me an offer I can't refuse",]
+        
+    ],
+    [
+        r"(.*) (location|city) ?",
+        ['Bangalore',]
+    ],
+    [
+        r"(.*)raining in (.*)",
+        ["No rain in the past 4 days here in %2","In %2 there is a 50% chance of rain",]
+    ],
+    [
+        r"how (.*) health (.*)",
+        ["Health is very important, but I am a computer, so I don't need to worry about my health ",]
+    ],
+    [
+        r"(.*)(sports|game|sport)(.*)",
+        ["I'm a very big fan of Cricket",]
+    ],
+    [
+        r"who (.*) (Cricketer|Batsman)?",
+        ["Dhoni"]
+    ],
+    [
+        r"quit",
+        ["Bye for now. See you soon :) "]
+    ],
+    [
+        r"(.*)",
+        ['That is nice to hear']
+    ],
+]
+
+print("Start a conversation. Type quit to leave ")
+chat = Chat(pairs, reflections)
+chat.converse()
